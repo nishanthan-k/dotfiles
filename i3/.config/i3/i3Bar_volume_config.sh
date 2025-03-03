15e5e7cecf83e566e3ce5300b249484a36be458c
Nishanthan K <89657905+nishanthan-k@users.noreply.github.com>
2025-03-03 23:51:43 +0530

    Delete i3/.config/i3/i3Bar_volume_config.sh

--- i3/.config/i3/i3Bar_volume_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start i3status and get the output
-i3status | while IFS= read -r line; do
-  # Append volume to the i3status output
-  volume=$(pamixer --get-volume-human)
-  echo "$line | Vol: $volume"
-done
